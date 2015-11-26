d6a2376c70c5bf21857cc4bd2047b86e20edcd29
Irene Araya <iarayal>
2015-11-25 20:04:01 -0600

    patrones

--- Practicas/19-Patrones/js/app.js
+++ Practicas/19-Patrones/js/app.js
@@ -1,15 +1,16 @@
 var app = angular.module('patrones', ['ngRoute']);
+
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 
 	.when('/', {
-		templateUrl: 'views/home.html'
+		templateUrl: '../views/home.html'
 	})
 	.when('/categorias', {
-		templateUrl: 'views/categorias.html'
+		templateUrl: '../views/categorias.html'
 	})
 	.when('/provincias', {
-		templateUrl: 'views/provincias.html'
+		templateUrl: '../views/provincias.html'
 	})
 	.otherwise({
 		redirectTo: '/'
